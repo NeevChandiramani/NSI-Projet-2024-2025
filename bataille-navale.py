6aad10f1f614326dc8c2680e6f05e10ac2964a59
Maaaxxxxxxxx <168303233+Maaaxxxxxxxx@users.noreply.github.com>
2024-10-19 21:12:07 +0200

    Update bataille-navale.py

--- bataille-navale.py
+++ bataille-navale.py
@@ -1,1 +1,29 @@
-print(2)
+class Joueur :
+    def __init__ (self, nom) :
+        self.nom = nom
+
+    def choix_nom (self) :
+        self.nom = input("Quel pseudo voulez-vous choisir ?")
+        return    
+        
+
+class Bateau :
+    def __init__ (self, L, l, vie) :
+        self.longueur = L
+        self.largeur = l
+        self.vie = vie
+
+
+#bateaux à définir
+
+
+class Grille_def :
+    def __init__ (self, x, y) :
+        self.abscisse = x
+        self.ordonnee = y
+
+
+class Grille_atk :
+    def __init__ (self, x, y) :
+        self.abscisse = x
+        self.ordonnee = y
